5d0e3bef17f6b4b88f4c67c8b5c8c02d72375f9a
Raymond Oung <r.oung@hapi-robo.com>
2020-11-18 14:23:54 +0900

    Removes unneccessary code

--- light.py
+++ light.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Test Motor
-
-When using daemon, remember to set the environment variables:
-export PIGPIO_ADDR=HOSTNAME
-export PIGPIO_PORT=PORT (Default: 8888)
+"""Remotely control light
 
 """
 import pigpio
-import argparse
 
 from time import sleep
 
 
+# remote host parameters
 # HOSTNAME = '192.168.0.128' # test-bench
-HOSTNAME = '192.168.0.152' # raybot
+HOSTNAME = '192.168.0.152' # robot
 PORT = 8888
 
+# PIGPIO parameters
 GPIO_PIN = 17 # https://pinout.xyz/pinout/pin11_gpio17#
 
 
@@ -27,14 +24,9 @@ def push_button(pin):
 
 
 if __name__ == "__main__":
-
-    # PARSER = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    # PARSER.add_argument("duty_cycle", type=int, help="Duty-cycle [0-100]")
-    # ARGS = PARSER.parse_args()
-
     pi = pigpio.pi(HOSTNAME, PORT)
 
-    # exit script if unable to connect
+    # exit script if unable to connect to remote host
     if not pi.connected:
         print("Unable to connect to {}:{}".format(HOSTNAME, PORT))
         sys.exit(1)
